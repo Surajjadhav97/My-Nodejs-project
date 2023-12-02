ee68dab2e9b4248baa7e18c5c6684361b94c1c53
root <root@ip-172-31-34-27.us-east-2.compute.internal>
2023-12-02 14:56:38 +0000

    Initial Commit

---
+++ My-Nodejs-project/nodejs-web-server/index.js
@@ -0,0 +1,13 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Welcome to my Node.js web server!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
